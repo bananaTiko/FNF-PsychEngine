56df52aec28c09c2814a042fe5a4d5b4c38a413a
bananaTiko <111482265+bananaTiko@users.noreply.github.com>
2024-10-07 16:53:40 -0700

    lime fix

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -589,6 +589,8 @@ class TitleState extends MusicBeatState
 	override function beatHit()
 		{
 			super.beatHit();
+			
+			FlxG.camera.zoom += 0.015;
 	
 			if (logoBl != null)
 				logoBl.animation.play('bump', true);
