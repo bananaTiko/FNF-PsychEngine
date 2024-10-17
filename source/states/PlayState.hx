a305581e43c77b552b0cc9acea1d5cea8b5f70c9
bananaTiko <111482265+bananaTiko@users.noreply.github.com>
2024-10-16 18:46:40 -0700

    Lets hope i fixed it GOD

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -683,26 +683,24 @@ if(eventNotes.length < 1) checkEventNote();
 		#end
 		return playbackRate;
 	}
-}
 
 	#if (LUA_ALLOWED || HSCRIPT_ALLOWED)
-	public function addTextToDebug(text:String, color:FlxColor) 
-	{
-		var newText:psychlua.DebugLuaText = luaDebugGroup.recycle(psychlua.DebugLuaText);
-		newText.text = text;
-		newText.color = color;
-		newText.disableTime = 6;
-		newText.alpha = 1;
-		newText.setPosition(10, 8 - newText.height);
+    public function addTextToDebug(text:String, color:FlxColor):Void {
+        var newText:psychlua.DebugLuaText = luaDebugGroup.recycle(psychlua.DebugLuaText);
+        newText.text = text;
+        newText.color = color;
+        newText.disableTime = 6;
+        newText.alpha = 1;
+        newText.setPosition(10, 8 - newText.height);
 
-		luaDebugGroup.forEachAlive(function(spr:psychlua.DebugLuaText) {
-			spr.y += newText.height + 2;
-		});
-		luaDebugGroup.add(newText);
-		Sys.println(text);
-	}
+        luaDebugGroup.forEachAlive(function(spr:psychlua.DebugLuaText) {
+            spr.y += newText.height + 2;
+        });
+        luaDebugGroup.add(newText);
+        Sys.println(text);
+    }
 }
-	#end
+#end
 
 	public function reloadHealthBarColors() {
 		healthBar.setColors(FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
